94a80379c0fc9fa4b5bf7d4bf8f90f14d2ac8a6c
Mateus Paiva <mattheuspaaiva@gmail.com>
2022-04-28 16:25:12 -0300

    desafio 3 - lendo arquivo com o promise

---
+++ CursoJS/funções/desafio_3.js
@@ -0,0 +1,19 @@
+//lendo o arquivo com o promise e algumas transformações com as strings
+const fs = require('fs')
+const path = require('path')
+
+
+function lerArquivo(caminho){
+    return new Promise(resolve => {
+        fs.readFile(caminho, function(_, conteudo){
+            resolve(conteudo.toString())
+        })
+        console.log('Depois de ler')
+    })
+}
+const caminho =  path.join(__dirname, 'dados.txt')
+lerArquivo(caminho)
+    .then(conteudo => conteudo.split('\n'))
+    .then(linhas => linhas.join(','))
+    .then(conteudo => `O valor final é: ${conteudo}`)
+    .then(console.log)
