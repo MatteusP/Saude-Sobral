96cbfaf148b6fb978393f0480cb0d1363b7f8ca7
Mateus Paiva <mattheuspaaiva@gmail.com>
2022-05-04 09:27:48 -0300

    separando as linhas

--- CursoJS/Projeto_1/index.js
+++ CursoJS/Projeto_1/index.js
@@ -6,4 +6,7 @@ const caminho = path.join(__dirname, '..', 'Dados', 'legendas')
 fn.lerDiretorio(caminho)
     .then(arquivos => fn.elementosTerminadosCom(arquivos, '.srt'))
     .then(arquivosSRT => fn.lerArquivos(arquivosSRT))
+    .then(conteudos => conteudos.join(''))
+    .then(todoConteudo => todoConteudo.split('\n'))
+
     .then(console.log)
