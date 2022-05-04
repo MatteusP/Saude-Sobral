64e0e8a111d69f185e64f7e41766eedc92e18c0b
Mateus Paiva <mattheuspaaiva@gmail.com>
2022-05-04 08:54:54 -0300

    Lendo arquivos do diretorio,usando promise, filtrando arquivos por extensão

--- CursoJS/Projeto_1/funcoes.js
+++ CursoJS/Projeto_1/funcoes.js
@@ -1,9 +1,25 @@
 const fs = require('fs')
+const path = require('path')
 
 function lerDiretorio(caminho) {
-    let arquivos = fs.readdirSync(caminho)
-    console.log(arquivos)
+    return new Promise((resolve, reject) => {
+        //caso a leitura seja bem sucedida os nov=mes dos arquivos serão retonados
+        try {
+            let arquivos = fs.readdirSync(caminho)
+            arquivos = arquivos.map(arquivo => path.join(caminho, arquivo))
+            resolve(arquivos)
+
+        }catch(e){
+            reject(e)
+            
+        }
+    })
 }
+function elementosTerminadosCom(array, padrao){
+    return array.filter(el => el.endsWith(padrao))
+}
+
 module.exports = {
-    lerDiretorio
+    lerDiretorio,
+    elementosTerminadosCom
 }
--- CursoJS/Projeto_1/index.js
+++ CursoJS/Projeto_1/index.js
@@ -1,5 +1,8 @@
 const path = require('path')
-const fn = require(`./funcoes`)
+const fn = require('./funcoes')
 
-const caminho = path.join(__dirname, 'legendas'  )
-fn.lerDiretorio()
+const caminho = path.join(__dirname, '..', 'Dados', 'legendas')
+
+fn.lerDiretorio(caminho)
+    .then(arquivos => fn.elementosTerminadosCom(arquivos, '.srt'))
+    .then(console.log)
