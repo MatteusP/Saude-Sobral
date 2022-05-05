56268d884244c220f5ceb6bb3ce211e5a4054d20
Mateus Paiva <mattheuspaaiva@gmail.com>
2022-05-05 08:59:09 -0300

    ajuste agrupar palavras

--- CursoJS/Projeto_1/index.js
+++ CursoJS/Projeto_1/index.js
@@ -17,15 +17,13 @@ const separarPorpalavras = todoConteudo => todoConteudo.split(' ')
 
 //
 function agruparPalavras(palavras){
-    return palavras.reduce((agrupamento, palavra) => {
-        const p = palavra.toLowerCase()
-        if(agrupamento[p]){
-            agrupamento[p] += 1
-        }else{
-            agrupamento[p] = 1
-        }
-        return agrupamento
-    }, {})
+    return Object.values(palavras.reduce((acc, palavra) => {
+        const el = palavra.toLowerCase()
+        const qtde = acc[el] ? acc[el].qtde + 1: 1
+        acc[el] = { elemento: el, qtde}
+
+        return acc
+    }, {}))
 }
 
 
